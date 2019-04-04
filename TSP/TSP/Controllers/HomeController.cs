a6e1e00af5a20648d12626202284ee4737f7f2b1
Amit Jaiswal <amitjaiswal103@gmail.com>
2019-04-04 20:20:26 +0530

    First commit

---
+++ TSP/TSP/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TSP.Models;
+
+namespace TSP.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About(LogInModel logInModel)
+        {
+            if (logInModel.Mobile != null)
+            {
+               
+            }
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult CommonLogIn()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
